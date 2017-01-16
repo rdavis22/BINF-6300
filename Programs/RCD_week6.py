7a22e24576b9c84a2ae47f6f5a080764f79d0361
Richard Davis <rdavis22@tulane.edu>
2017-01-16 20:33:06 +0000

    week 6 program

---
+++ Programs/RCD_week6.py
@@ -0,0 +1,10 @@
+#Acetaminophen
+dose_mg=raw_input('What is your dose in mg?\n')
+new_dose=int(dose_mg)
+print new_dose
+fq_day=raw_input('How many times a day do you take your Acetaminophen?\n')
+new_fq=int(fq_day)
+print new_fq
+age_yrs=raw_input('What is your age in years?\n')
+new_age=int(age_yrs)
+print new_age
